510aee71273be27a0271c3b0d10043ef1bf3f2d5
Tisha N <tnai255@aucklanduni.ac.nz>
2023-08-12 22:57:03 +1200

    Created buttons and background ui.

--- munch-match/src/App.js
+++ munch-match/src/App.js
@@ -1,22 +1,19 @@
-
+import "./App.css"
 //imports the button component
 import ExampleButton from "./components/exampleButtons.jsx"
+import fork from "./images/fork.png"
+import knife from "./images/knife.png"
 
 //The example button component is used in the code below
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <ExampleButton />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MunchMatch</h1>
       </header>
+      <img class="knife" src={knife} alt="knife"></img>
+      <ExampleButton/>
+      <img class="fork" src={fork} alt="fork"></img>
     </div>
   );
 }
