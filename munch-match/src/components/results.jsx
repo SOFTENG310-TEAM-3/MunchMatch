7203631f33a050f6a1e378b2017f107826502152
Tanishq <tanishq.ranjith00@gmail.com>
2023-08-19 10:14:10 +1200

    Refactored the repeated code in results component

--- munch-match/src/components/results.jsx
+++ munch-match/src/components/results.jsx
@@ -2,6 +2,7 @@
 import { Component } from "react";
 import "../App.css"
 import Rating from "./rating";
+import ResultCard from "./resultCard"
 import dessert from "../images/dessert.png"
 import { getResults } from "../Results.js"
 
@@ -38,158 +39,14 @@ class Results extends Component{
         //To export multiple components, surround it with a <div> tag
         <div>
             <div className="resultsRow">
-                <div className="button results">
-
-                
-                    <div className="resultsHeader">
-                        <h3>{results[0]?.name}</h3>
-                        <h5>10 KM | {Array(results[0]?.price).fill('$').join('')}</h5>
-                    </div>
-                
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[0]?.maps}  target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[0]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[0]?.rating} total={results[0]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[0]?.openingHours}</h5>
-                    <h5>Address: {results[0]?.formattedAddress}</h5>
-                    <h5>Phone: {results[0]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
-                <div className="button results">
-
-                    {results.length > 0 && (
-                        <div className="resultsHeader">
-                            <h3>{results[1]?.name}</h3>
-                            <h5>10 KM | {Array(results[1]?.price).fill('$').join('')}</h5>
-                        </div>
-                    )}
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[1]?.maps} target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[1]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[1]?.rating} total={results[1]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[1]?.openingHours}</h5>
-                    <h5>Address: {results[1]?.formattedAddress}</h5>
-                    <h5>Phone: {results[1]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
-                <div className="button results">
-
-                    {results.length > 0 && (
-                        <div className="resultsHeader">
-                            <h3>{results[2]?.name}</h3>
-                            <h5>10 KM | {Array(results[2]?.price).fill('$').join('')}</h5>
-                        </div>
-                    )}
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[2]?.maps} target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[2]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[2]?.rating} total={results[2]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[2]?.openingHours}</h5>
-                    <h5>Address: {results[2]?.formattedAddress}</h5>
-                    <h5>Phone: {results[2]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
+                <ResultCard result={results[0]}/>
+                <ResultCard result={results[1]}/>
+                <ResultCard result={results[2]}/>
             </div>
             <div className="resultsRow">
-                <div className="button results">
-
-                    {results.length > 0 && (
-                        <div className="resultsHeader">
-                            <h3>{results[3]?.name}</h3>
-                            <h5>10 KM | {Array(results[3]?.price).fill('$').join('')}</h5>
-                        </div>
-                    )}
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[3]?.maps} target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[3]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[3]?.rating} total={results[3]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[3]?.openingHours}</h5>
-                    <h5>Address: {results[3]?.formattedAddress}</h5>
-                    <h5>Phone: {results[3]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
-                <div className="button results">
-
-                    {results.length > 0 && (
-                        <div className="resultsHeader">
-                            <h3>{results[4]?.name}</h3>
-                            <h5>10 KM | {Array(results[4]?.price).fill('$').join('')}</h5>
-                        </div>
-                    )}
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[4]?.maps} target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[4]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[4]?.rating} total={results[4]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[4]?.openingHours}</h5>
-                    <h5>Address: {results[4]?.formattedAddress}</h5>
-                    <h5>Phone: {results[4]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
-                <div className="button results">
-
-                    {results.length > 0 && (
-                        <div className="resultsHeader">
-                            <h3>{results[5]?.name}</h3>
-                            <h5>10 KM | {Array(results[5]?.price).fill('$').join('')}</h5>
-                        </div>
-                    )}
-
-                    <div className="actionContainer">
-                    <button className="actionButton"><a href={results[5]?.maps} target="_blank"><h4>DIRECTIONS</h4></a></button>
-                    <button className="actionButton"><a href={results[5]?.website} target="_blank"><h4>WEBSITE</h4></a></button>
-                    <button className="actionButton"><h4>CALL</h4></button>
-                    </div>
-
-                    <div className="infoContainer">
-                    <h5 className="rating"> <Rating value={results[5]?.rating} total={results[5]?.totalRatings}/> </h5>
-                    <h4>Services: </h4>
-                    <h5>Hours: {results[5]?.openingHours}</h5>
-                    <h5>Address: {results[5]?.formattedAddress}</h5>
-                    <h5>Phone: {results[5]?.formattedPhone}</h5>
-                    </div>
-                    
-                    <img className="resultImage" src={dessert} alt="dessert"></img>
-                </div>
+                <ResultCard result={results[3]}/>
+                <ResultCard result={results[4]}/>
+                <ResultCard result={results[5]}/>
             </div>
             <div>
             <button className="button" onClick={() => this.onBackClick()} style={{width: "30%"}}><h3>Choose Again</h3></button>
