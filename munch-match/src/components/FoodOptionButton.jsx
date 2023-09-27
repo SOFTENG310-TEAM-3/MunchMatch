eae1a2de8cc377b522db7cb93ce562d84cfffb7e
arielyxw <wany468@aucklanduni.ac.nz>
2023-09-27 16:39:53 +1300

    Resolved PR comments removing redundant styling and missed classes

--- munch-match/src/components/FoodOptionButton.jsx
+++ munch-match/src/components/FoodOptionButton.jsx
@@ -3,8 +3,8 @@ import styles from "./FoodOptionButton.module.css"
 const FoodOptionButton = ({ foodOption, disabled, onClick }) => {
 
     return (
-        <button className={`${styles.button} ${disabled ? "disabledButton" : ""}`} disabled={disabled} onClick={() => onClick(foodOption)}>
-            <img className={`${styles.food} ${disabled ? "disabled" : ""}`} src={require(`../images/${foodOption}.png`)} alt={foodOption} />
+        <button className={`${styles.button} ${disabled ? styles.disabledButton : ""}`} disabled={disabled} onClick={() => onClick(foodOption)}>
+            <img className={`${styles.food} ${disabled ? styles.disabled : ""}`} src={require(`../images/${foodOption}.png`)} alt={foodOption} />
         </button>
     );
 }
