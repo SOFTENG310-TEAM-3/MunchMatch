df5e90617809132b8ca00b8ca8487cce80d407bd
Ariel <100349004+arielyxw@users.noreply.github.com>
2023-10-17 11:23:29 +1300

    Added a few more quiz questions and removed a duplicate method (#87)

--- munch-match/src/data/quizQuestions.js
+++ munch-match/src/data/quizQuestions.js
@@ -9,17 +9,31 @@ const quizQuestions = [
             },
             {
                 answer: "Savoury",
-                disables: ["fruit", "dessert", "bakery"],
+                disables: ["fruit", "dessert"],
             }
         ],
     },
     {
         id: "1",
+        question: "Hot or Cold?",
+        options: [
+            {
+                answer: "Hot",
+                disables: ["dessert", "fruit", "sushi"],
+            },
+            {
+                answer: "Cold",
+                disables: ["burger", "pizza", "chicken"],
+            }
+        ],
+    },
+    {
+        id: "2",
         question: "Big Feed or Little Treat?",
         options: [
             {
                 answer: "Big Feed",
-                disables: ["dessert", "fruit"],
+                disables: ["dessert", "fruit", "bakery"],
             },
             {
                 answer: "Little Treat",
@@ -27,6 +41,34 @@ const quizQuestions = [
             }
         ],
     },
+    {
+        id: "3",
+        question: "Healthy or Cheat Day?",
+        options: [
+            {
+                answer: "Healthy",
+                disables: ["dessert", "burger", "pizza", "chicken"],
+            },
+            {
+                answer: "Cheat Day",
+                disables: ["fruit"],
+            }
+        ],
+    },
+    {
+        id: "4",
+        question: "Vegetarian/Vegan?",
+        options: [
+            {
+                answer: "No",
+            },
+            {
+                answer: "Yes",
+                disables: ["chicken"],
+            }
+        ],
+    }
+
 ];
 
 export default quizQuestions;
